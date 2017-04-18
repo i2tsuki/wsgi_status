d00cd0a8af5def1471224e4ebbb79b02d39cdce1
kizkoh <mac-daisuki@live.jp>
2017-04-18 16:37:03 +0900

    Stabilize license

--- setup.py
+++ setup.py
@@ -6,19 +6,18 @@ from setuptools import setup, find_packages
 with open('README.rst') as f:
     readme = f.read()
 
-with open('LICENSE') as f:
-    license = f.read()
-
 setup(
     name='wsgi_status',
     version='0.1.0',
+
     description='Monitoring status wsgi upper middleware',
     long_description=readme,
     author='kizkoh',
     author_email='mac-daisuki@live.com',
+    license='MIT',
     install_requires=['psutil'],
     url='https://github.com/kizkoh/wsgi_status',
     license=license,
-    packages=find_packages(exclude=('tests', 'docs')),
+    packages=find_packages(exclude=('examples', 'tests', 'docs')),
     test_suite='tests'
 )
