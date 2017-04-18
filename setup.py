8b42b8f77e32db70fe88e9d8989bc5a52d050e9e
kizkoh <mac-daisuki@live.jp>
2017-04-18 16:32:09 +0900

    Fix syntax

--- setup.py
+++ setup.py
@@ -19,6 +19,6 @@ setup(
     install_requires=['psutil'],
     url='https://github.com/kizkoh/wsgi_status',
     license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    packages=find_packages(exclude=('tests', 'docs')),
     test_suite='tests'
 )
