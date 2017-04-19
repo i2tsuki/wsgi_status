e9545b18176202b92d9793a8c6708ddb6be80dd5
kizkoh <mac-daisuki@live.jp>
2017-04-19 15:57:38 +0900

    Fix example

--- example.sh
+++ example.sh
@@ -3,8 +3,9 @@
 set -eu
 
 python -m venv ./venv
+./venv/bin/pip install -r ./requirements_test.txt
 ./venv/bin/python ./setup.py develop
 
 cd ./examples
-./venv/bin/gunicorn -c gunicorn.conf.py upperware:app
+../venv/bin/gunicorn -c gunicorn.conf.py upperware:app
 cd ${OLDPWD}
